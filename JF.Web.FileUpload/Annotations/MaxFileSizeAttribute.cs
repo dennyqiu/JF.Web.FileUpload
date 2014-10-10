f9458299df73e2fb6969c1c3dce3301119fc2b88
Josef Fazekas <j.fazekas@theuntitled.eu>
2014-10-10 02:49:07 +0200

    Added Code

---
+++ JF.Web.FileUpload/Annotations/MaxFileSizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web;
+
+namespace JF.Web.FileUpload.Annotations {
+
+	public class MaxFileSizeAttribute : ValidationAttribute {
+
+		private readonly int _maxFileSize;
+
+		public MaxFileSizeAttribute( int maxMbFileSize ) {
+			_maxFileSize = maxMbFileSize * 8 * 1024 * 1024;
+		}
+
+		public override bool IsValid( object value ) {
+			var file = value as HttpPostedFileBase;
+
+			if ( file == null ) {
+				return true;
+			}
+
+			return file.ContentLength <= _maxFileSize;
+		}
+
+		public override string FormatErrorMessage( string name ) {
+			return base.FormatErrorMessage( _maxFileSize.ToString( CultureInfo.InvariantCulture ) );
+		}
+
+	}
+
+}
---
+++ JF.Web.FileUpload/Annotations/ValidFileExtensionsAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JF.Web.FileUpload.Annotations {
+
+	public class ValidFileExtensionsAttribute : DataTypeAttribute , IClientValidatable {
+
+		private readonly FileExtensionsAttribute _innerAttribute = new FileExtensionsAttribute();
+
+		public ValidFileExtensionsAttribute() : base( DataType.Upload ) {
+			ErrorMessage = _innerAttribute.ErrorMessage;
+		}
+
+		public string Extensions {
+			get {
+				return _innerAttribute.Extensions;
+			}
+			set {
+				_innerAttribute.Extensions = value;
+			}
+		}
+
+		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(
+			ModelMetadata metadata ,
+			ControllerContext context ) {
+			var rule = new ModelClientValidationRule {
+				ValidationType = "accept" ,
+				ErrorMessage = ErrorMessage
+			};
+			rule.ValidationParameters["exts"] = _innerAttribute.Extensions;
+			yield return rule;
+		}
+
+		public override string FormatErrorMessage( string name ) {
+			return _innerAttribute.FormatErrorMessage( name );
+		}
+
+		public override bool IsValid( object value ) {
+			var file = value as HttpPostedFileBase;
+			if ( file != null ) {
+				return _innerAttribute.IsValid( file.FileName );
+			}
+
+			return _innerAttribute.IsValid( value );
+		}
+
+	}
+
+}
